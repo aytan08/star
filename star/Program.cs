1fb233b3a154fac48fddd500a883bbbd5c06d4f5
aytan08 <aytanzd@code.edu.az>
2025-09-09 11:35:01 +0400

    star

---
+++ star/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+# region Task1
+/*int mark = 4;
+switch (mark)
+{ case 2:
+     Console.WriteLine("Pis");
+        break;
+
+    case 3:
+        Console.WriteLine("Kafi");
+        break;
+
+    case 4:
+        Console.WriteLine("Yaxşi");
+        break;
+
+    case 5:
+        Console.WriteLine("Əla");
+        break;
+
+    default:
+        Console.WriteLine("2-5 arası ədəd daxil edin");
+        break;
+
+}
+*/
+#endregion
+#region Task2
+/*int n = 10;
+int m = 70;
+int sum = 0;
+for (int i = n; i <= m; i++)
+{  if (i % 10 == 0)
+    { sum += i; }
+}
+Console.WriteLine("Cem:" + sum); */
+#endregion
+#region Task3
+/*int M = 30;
+int count = 0;
+for (int i = 1; i <= M; i++)
+    {
+        if (i % 3 == 0)
+        {
+            count++;
+                }
+    }
+    Console.WriteLine("3-e bolunen ededlerin sayi:" +count); */
+#endregion
+#region Task4
+/*int M = 45;
+int count = 0;
+int sum = 0;
+for (int i=1; i<= M; i++)
+{ if (i % 5 == 0)
+        { count++;
+            sum += i;
+        }
+    }
+if (count > 0)
+{ Console.WriteLine((double)sum / count); }
+else
+{ Console.WriteLine("5-e bolunen eded yoxdur"); } */
+#endregion
+#region Task5
+/*int a = 64;
+Console.WriteLine(a+ "ededinin bolenleri:");
+for (int i = 1; i <= a; i++)
+{ if (a % i == 0)
+    { Console.WriteLine( i ); }
+} */
+#endregion
+#region Task6
+/*int x = 20;
+int y = 5;
+int result = -1;
+for (int i = 0; i <= x; i++)
+{
+    if (i % y == 0)
+    { result = i; }
+}
+Console.WriteLine(result); */
+#endregion
+#region Task7
+/*
+int a = 125;
+if (a < 0)
+{ Console.WriteLine("-"); }
+string str = a.ToString();
+for (int i=0; i < str.Length; i++)
+{ Console.WriteLine (str[i]); } */
+#endregion
+int
+
+
